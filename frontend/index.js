d1aeb03b44eb45cb0c1b9c69c043ef21e22ae324
dralucard666 <j.brossmann@web.de>
2020-08-13 11:28:44 +0200

    #dumbpc

--- frontend/index.js
+++ frontend/index.js
@@ -72,6 +72,9 @@ var blacklist = [["B",1,0],["B",1,1],["B",1,2],["B",1,3],["B",1,4],["B",1,5],["B
 refigureboard();
 
 function refigureboard() {
+
+
+
 whitelist.forEach(element => {
   document.getElementById(arraynuminid(element[1],element[2])).innerText=element[0];
   document.getElementById(arraynuminid(element[1],element[2])).style.color = "#ff0000";
@@ -90,21 +93,15 @@ function movefigure(id){
 
 
   if (currentfiguretomove!=null) {
-    if (moveispossible(parseInt(id.substring(0,1)),parseInt(id.substring(1,2)))) {
-      
+    if (moveispossible(currentfiguretomove[0], currentfiguretomove[1], currentfiguretomove[2], parseInt(id.substring(0,1)),parseInt(id.substring(1,2)))) {
 
-     if (turn%2==0) {
-      lista=whitelist;
-      listb=blacklist;
-     } else {
-       lista=blacklist;
-       listb=whitelist;
-     }
-
-     lista.forEach(element => {
+     whitelist.forEach(element => {
         if(element[1]==currentfiguretomove[1] && element[2]==currentfiguretomove[2]) {
           element[1]=parseInt(id.substring(0,1));
            element[2]=parseInt(id.substring(1,2));
+           if (element[0]==="B" && parseInt(id.substring(0,1))==0) {
+              element[0]="D";
+           }
         }
       });
       document.getElementById(""+currentfiguretomove[1]+currentfiguretomove[2]).innerText=null;
@@ -114,7 +111,7 @@ function movefigure(id){
 
 
 
-     listb.forEach(element => {
+     blacklist.forEach(element => {
 
         if(element[1]==parseInt(id.substring(0,1)) && element[2]==parseInt(id.substring(1,2))) {
           figurekilled=figurekilledcounter;
@@ -123,7 +120,7 @@ function movefigure(id){
         figurekilledcounter++;
         });
         if (figurekilled>-1) {
-          listb.splice(figurekilled,1)
+          blacklist.splice(figurekilled,1)
         }
 
         figurekilled=-1;
@@ -133,6 +130,7 @@ function movefigure(id){
 turn+=1;
 
 refigureboard();
+computermove();
 
     } else {
       if (turn%2==0) {
@@ -152,6 +150,157 @@ currentfiguretomove=[document.getElementById(id).innerText, parseInt(id.substrin
 
 }
 
+
+function evaluatemove(x,y) {
+
+  if (turn%2==0) {
+    lista=whitelist;
+    listb=blacklist;
+    } else {
+    lista=blacklist;
+    listb=whitelist;
+    }
+
+  let figurekilled=-1;
+  let figurekilledcounter=0;
+  let killedfiguretype= null;
+  let killedfigurex= null;
+  let killedfigurey= null;
+
+   
+  listb.forEach(element => {
+  
+    if(element[1]==x && element[2]==y) {
+      figurekilled=figurekilledcounter;
+    }
+    figurekilledcounter++;
+    });
+    if (figurekilled>-1) {
+      killedfiguretype=listb[figurekilled][0];
+      killedfigurex=listb[figurekilled][1];
+      killedfigurey=listb[figurekilled][2];
+      listb.splice(figurekilled,1)
+    }
+  
+    figurekilled=-1;  
+
+
+
+
+let whitevalue=0;
+let blackvalue=0;
+
+  listb.forEach(element => {
+    if(element[0]=="B") {  whitevalue+=1  } 
+    else if (element[0]=="T") {  whitevalue+=5  }
+    else if (element[0]=="S") {   whitevalue+=3 }
+    else if (element[0]=="L") {  whitevalue+=3  }
+    else if (element[0]=="K") {  whitevalue+=39  }
+    else {whitevalue+=8}
+  });
+
+  lista.forEach(element => {
+    if(element[0]=="B") {  blackvalue++  } 
+    else if (element[0]=="T") {  blackvalue+=5  }
+    else if (element[0]=="S") {   blackvalue+=3 }
+    else if (element[0]=="L") {  blackvalue+=3  }
+    else if (element[0]=="K") {  blackvalue+=39  }
+    else {blackvalue+=8}
+  });
+
+  if(killedfiguretype!=null) {
+listb.push([killedfiguretype,killedfigurex,killedfigurey])
+  }
+
+return blackvalue-whitevalue;
+}
+
+
+function computermove()  {
+
+  let lista= null;
+  let listb= null;
+
+if (turn%2==0) {
+lista=whitelist;
+listb=blacklist;
+} else {
+lista=blacklist;
+listb=whitelist;
+}
+
+
+  var bestcomputermoveval=-80;
+var currentbestfigure = null;
+var waspawn = false;
+
+  lista.forEach(element => {
+    for (let x=0 ; x<8 ; x++) {
+      for (let y=0 ; y<8 ; y++) {
+
+
+        if (moveispossible(element[0],element[1],element[2],x,y)){  
+          
+          if (element[0]==="B" && x==7) {
+            element[0]="D";
+            waspawn=true;
+         }
+     
+          if(evaluatemove(x,y)>bestcomputermoveval){
+            console.log("derzeit bester move"+bestcomputermoveval);
+            console.log("neuer bester move"+evaluatemove(x,y));
+            bestcomputermoveval=evaluatemove(x,y);
+            currentbestfigure = [element[1],element[2],x,y];
+          }
+          if (waspawn==true) {
+            waspawn=false;
+            element[0]="B";
+          }
+        }
+      }
+    }
+})
+
+
+
+
+lista.forEach(element => {
+  if(element[1]==currentbestfigure[0] && element[2]==currentbestfigure[1]) {
+    element[1]=currentbestfigure[2];
+     element[2]=currentbestfigure[3];
+     if (element[0]==="B" && currentbestfigure[2]==7) {
+      element[0]="D";
+   }
+  }
+});
+document.getElementById(""+currentbestfigure[0]+currentbestfigure[1]).innerText=null;
+
+let figurekilled=-1;
+let figurekilledcounter=0;
+
+
+
+listb.forEach(element => {
+
+  if(element[1]==currentbestfigure[2] && element[2]==currentbestfigure[3]) {
+    figurekilled=figurekilledcounter;
+
+  }
+  figurekilledcounter++;
+  });
+  if (figurekilled>-1) {
+    listb.splice(figurekilled,1)
+  }
+
+  figurekilled=-1;
+
+currentfiguretomove=null;
+turn+=1;
+
+
+refigureboard();
+
+}
 
 function enemyfigureonfield(x,y) {
 
@@ -187,15 +336,15 @@ function friendlyfigureonfield(x,y) {
 }
 
 
-function moveispossible(x,y) {
+function moveispossible(type, currentpositionx, currentpositiony,x,y) {
 
-if (currentfiguretomove[0]=="B") {
+if (type[0]=="B") {
   if (turn%2==0) {
 
 
 
-     if (((currentfiguretomove[1]-1==x || (currentfiguretomove[1]-2==x && currentfiguretomove[1]==6)) && currentfiguretomove[2]==y) 
-     || (currentfiguretomove[1]-1==x && (currentfiguretomove[2]==y-1 || currentfiguretomove[2]-1==y) && enemyfigureonfield(x,y))) {
+     if (((currentpositionx-1==x || (currentpositionx-2==x && currentpositionx==6)) && currentpositiony==y && !enemyfigureonfield(x,y)&&!friendlyfigureonfield(x,y)) 
+     || (currentpositionx-1==x && (currentpositiony==y-1 || currentpositiony-1==y) && enemyfigureonfield(x,y))) {
 
        return true;
      }
@@ -206,8 +355,8 @@ if (currentfiguretomove[0]=="B") {
 
 
 
-    if ((currentfiguretomove[1]==x-1 || (currentfiguretomove[1]==x-2 && currentfiguretomove[1]==1))&& currentfiguretomove[2]==y 
-    || currentfiguretomove[1]==x-1 && (currentfiguretomove[2]==y-1 || currentfiguretomove[2]-1==y) && enemyfigureonfield(x,y)
+    if (((currentpositionx==x-1 || (currentpositionx==x-2 && currentpositionx==1))&& currentpositiony==y && !enemyfigureonfield(x,y)&&!friendlyfigureonfield(x,y))
+    || currentpositionx==x-1 && (currentpositiony==y-1 || currentpositiony-1==y) && enemyfigureonfield(x,y)
     ) {
 
       return true;
@@ -215,62 +364,59 @@ if (currentfiguretomove[0]=="B") {
 
     return false;
   }
-} else if (currentfiguretomove[0]=="T") {
-   if (currentfiguretomove[1]!=x && currentfiguretomove[2]!=y) {
-     console.log("ich bin in der trap")
+} else if (type[0]=="T") {
+   if (currentpositionx!=x && currentpositiony!=y) {
      return false;
    }
    let bool=true;
-  for (let z=currentfiguretomove[1]+1 ; z<x ; z++) {
-    console.log("hier bin ich z:"+z)
-    console.log(bool)
+  for (let z=currentpositionx+1 ; z<x ; z++) {
+
   bool=bool&&!enemyfigureonfield(z,y)&&!friendlyfigureonfield(z,y);
   }
-  for (let z=x+1 ; z<currentfiguretomove[1] ; z++) {
+  for (let z=x+1 ; z<currentpositionx ; z++) {
 
     bool=bool&&!enemyfigureonfield(z,y)&&!friendlyfigureonfield(z,y);
     }
-  for (let z=currentfiguretomove[2]+1 ; z<y ; z++) {
+  for (let z=currentpositiony+1 ; z<y ; z++) {
 
     bool=bool&&!enemyfigureonfield(x,z)&&!friendlyfigureonfield(x,z);
   }
-  for (let z=y+1 ; z<currentfiguretomove[2]  ; z++) {
+  for (let z=y+1 ; z<currentpositiony  ; z++) {
 
     bool=bool&&!enemyfigureonfield(x,z)&&!friendlyfigureonfield(x,z);
   }
 
   bool=bool&&!friendlyfigureonfield(x,y);
-  console.log(bool)
 
 
   return bool;
-} else if (currentfiguretomove[0]=="S") {
-  if (((currentfiguretomove[1]==x-2 || currentfiguretomove[1]-2==x) && (currentfiguretomove[2]==y-1 || currentfiguretomove[2]-1==y) &&!friendlyfigureonfield(x,y))
-  ||((currentfiguretomove[1]==x-1 || currentfiguretomove[1]-1==x) && (currentfiguretomove[2]==y-2 || currentfiguretomove[2]-2==y) &&!friendlyfigureonfield(x,y))) {
+} else if (type[0]=="S") {
+  if (((currentpositionx==x-2 || currentpositionx-2==x) && (currentpositiony==y-1 || currentpositiony-1==y) &&!friendlyfigureonfield(x,y))
+  ||((currentpositionx==x-1 || currentpositionx-1==x) && (currentpositiony==y-2 || currentpositiony-2==y) &&!friendlyfigureonfield(x,y))) {
 
     return true;
   }
 
   return false;
-}else if (currentfiguretomove[0]=="L") {
-  if (Math.abs(currentfiguretomove[1]-x)==Math.abs(currentfiguretomove[2]-y)&&!friendlyfigureonfield(x,y)) {
+}else if (type[0]=="L") {
+  if (Math.abs(currentpositionx-x)==Math.abs(currentpositiony-y)&&!friendlyfigureonfield(x,y)) {
 
    let bool = true;
 
-   for (let z=1 ; z<x-currentfiguretomove[1] ; z++) {
+   for (let z=1 ; z<x-currentpositionx ; z++) {
 
-  bool=bool&&!enemyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z)&&!friendlyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z);
+  bool=bool&&!enemyfigureonfield(currentpositionx+z,currentpositiony+z)&&!friendlyfigureonfield(currentpositionx+z,currentpositiony+z);
   }
 
-  for (let z=1 ; z<currentfiguretomove[1]-x ; z++) {
+  for (let z=1 ; z<currentpositionx-x ; z++) {
  
     bool=bool&&!enemyfigureonfield(x+z,y+z)&&!friendlyfigureonfield(x+z,y+z);
     }
-  for (let z=1 ; z<x-currentfiguretomove[2] ; z++) {
+  for (let z=1 ; z<x-currentpositiony ; z++) {
 
-    bool=bool&&!enemyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z)&&!friendlyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z);
+    bool=bool&&!enemyfigureonfield(currentpositionx+z,currentpositiony+z)&&!friendlyfigureonfield(currentpositionx+z,currentpositiony+z);
   }
-  for (let z=1 ; z<currentfiguretomove[2]-x  ; z++)   {  
+  for (let z=1 ; z<currentpositiony-x  ; z++)   {  
 
     bool=bool&&!enemyfigureonfield(x+z,y+z)&&!friendlyfigureonfield(x+z,y+z);
   }
@@ -279,61 +425,59 @@ if (currentfiguretomove[0]=="B") {
   }
 return false;
 
-} else if (currentfiguretomove[0]=="K") {
-  if (Math.abs(currentfiguretomove[1]-x)+Math.abs(currentfiguretomove[2]-y)==1&&!friendlyfigureonfield(x,y) 
-  || (Math.abs(currentfiguretomove[1]-x))==1 && Math.abs(currentfiguretomove[2]-y)==1 ) {
+} else if (type[0]=="K") {
+  if (Math.abs(currentpositionx-x)+Math.abs(currentpositiony-y)==1&&!friendlyfigureonfield(x,y) 
+  || (Math.abs(currentpositionx-x))==1 && Math.abs(currentpositiony-y)==1 ) {
 
     return true;
   }
 
   return false;
-}else if (currentfiguretomove[0]=="D") {
-  if (Math.abs(currentfiguretomove[1]-x)==Math.abs(currentfiguretomove[2]-y)&&!friendlyfigureonfield(x,y)) {
+}else if (type[0]=="D") {
+  if (Math.abs(currentpositionx-x)==Math.abs(currentpositiony-y)&&!friendlyfigureonfield(x,y)) {
 
     let bool = true;
  
-    for (let z=1 ; z<x-currentfiguretomove[1] ; z++) {
+    for (let z=1 ; z<x-currentpositionx ; z++) {
  
-   bool=bool&&!enemyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z)&&!friendlyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z);
+   bool=bool&&!enemyfigureonfield(currentpositionx+z,currentpositiony+z)&&!friendlyfigureonfield(currentpositionx+z,currentpositiony+z);
    }
  
-   for (let z=1 ; z<currentfiguretomove[1]-x ; z++) {
+   for (let z=1 ; z<currentpositionx-x ; z++) {
   
      bool=bool&&!enemyfigureonfield(x+z,y+z)&&!friendlyfigureonfield(x+z,y+z);
      }
-   for (let z=1 ; z<x-currentfiguretomove[2] ; z++) {
+   for (let z=1 ; z<x-currentpositiony ; z++) {
  
-     bool=bool&&!enemyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z)&&!friendlyfigureonfield(currentfiguretomove[1]+z,currentfiguretomove[2]+z);
+     bool=bool&&!enemyfigureonfield(currentpositionx+z,currentpositiony+z)&&!friendlyfigureonfield(currentpositionx+z,currentpositiony+z);
    }
-   for (let z=1 ; z<currentfiguretomove[2]-x  ; z++)   {  
+   for (let z=1 ; z<currentpositiony-x  ; z++)   {  
  
      bool=bool&&!enemyfigureonfield(x+z,y+z)&&!friendlyfigureonfield(x+z,y+z);
    }
  
      return bool;
-   } else if ((currentfiguretomove[1]==x && currentfiguretomove[2]!=y) || (currentfiguretomove[1]!=x && currentfiguretomove[2]==y)) {
+   } else if ((currentpositionx==x && currentpositiony!=y) || (currentpositionx!=x && currentpositiony==y)) {
    
    let bool=true;
-   for (let z=currentfiguretomove[1]+1 ; z<x ; z++) {
-     console.log("hier bin ich z:"+z)
-     console.log(bool)
+   for (let z=currentpositionx+1 ; z<x ; z++) {
+
    bool=bool&&!enemyfigureonfield(z,y)&&!friendlyfigureonfield(z,y);
    }
-   for (let z=x+1 ; z<currentfiguretomove[1] ; z++) {
+   for (let z=x+1 ; z<currentpositionx ; z++) {
  
      bool=bool&&!enemyfigureonfield(z,y)&&!friendlyfigureonfield(z,y);
      }
-   for (let z=currentfiguretomove[2]+1 ; z<y ; z++) {
+   for (let z=currentpositiony+1 ; z<y ; z++) {
  
      bool=bool&&!enemyfigureonfield(x,z)&&!friendlyfigureonfield(x,z);
    }
-   for (let z=y+1 ; z<currentfiguretomove[2]  ; z++) {
+   for (let z=y+1 ; z<currentpositiony  ; z++) {
  
      bool=bool&&!enemyfigureonfield(x,z)&&!friendlyfigureonfield(x,z);
    }
  
    bool=bool&&!friendlyfigureonfield(x,y);
-   console.log(bool)
  
    return bool;
  } else {
